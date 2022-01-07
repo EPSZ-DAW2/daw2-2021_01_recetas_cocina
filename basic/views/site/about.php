90ac2a6fd31fa96be0be1be03a3e4bf63df3d6cd
mluevi17 <mluevi17@usal.es>
2022-01-07 08:24:21 +0100

    Pagina de about.php creada con autores.

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -5,14 +5,29 @@
 use yii\helpers\Html;
 
 $this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+    <?php
+    $datos=[["Marcos Luengo Viñuela","descripcion", "correo"],["Nerea de Dios del Rio","descripcion", "correo"],["Fernando Jose Cofiño Gavito","descripcion", "correo"],["Victor Melado Barba","descripcion", "correo"],
+        ["Manuel Pardal Mollón","descripcion", "correo"],["Pablo","descripcion", "correo"],["Elsa","descripcion", "correo"],["Sara","descripcion", "correo"]];?>
 
-    <code><?= __FILE__ ?></code>
+    <h1 class="tituloCrud"><?= "Participantes del Proyecto" ?></h1>
+    <div class="row">
+        <?php foreach ($datos as $usuario){?>
+        <div class="card col-lg-4 my-3 text-center" style="border-radius: 25px;">
+            <img class="w-50 mx-auto" src="images/usuario.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?=$usuario[0]?></h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><?=$usuario[1]?></li>
+                <li class="list-group-item"><?=$usuario[2]?></li>
+            </ul>
+        </div>
+        <?php }?>
+    </div>
+
+
 </div>
