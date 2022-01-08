c70a07d6288cb7314821df36470030b71460007c
mluevi17 <mluevi17@usal.es>
2022-01-08 16:57:19 +0100

    Copia seguridad bbdd -- backups prueba.

--- basic/views/copia/index.php
+++ basic/views/copia/index.php
@@ -12,12 +12,34 @@ $this->title = Yii::t('app', 'Copia de seguridad');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="usuario-index">
+    <p class="btn btn-success w-100"><?php echo $msg;?></p>
 
 <h1 class="text-center w-100 rounded btn-verde"><?= Html::encode($this->title) ?></h1>
-    <h1><?php var_dump($msg);?></h1>
+
     <p>
+        <?= Html::a(Yii::t('app', 'Listado Copias de Seguridad'), ['index'], ['class' => 'btn btn-warning mt-3']) ?>
         <?= Html::a(Yii::t('app', 'Hacer Copia de Seguridad'), ['copia'], ['class' => 'btn btn-verde mt-3']) ?>
+        <?= Html::a(Yii::t('app', 'Descargar Copia SQL'), ['descargar'], ['class' => 'btn btn-primary mt-3']) ?>
         <?= Html::a(Yii::t('app', 'Restaurar Copia de Seguridad'), ['restaurarcopia'], ['class' => 'btn btn-danger mt-3']) ?>
     </p>
+    <?php
+    echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+    ['class' => 'yii\grid\SerialColumn'],
+    'nombre',
+    'ruta'
+    ],
+    'layout' => "\n{items}\n",
+    ]);
+    ?>
+
+    <div class="text-center w-100">
+        <?= LinkPager::widget([
+            'pagination' => $dataProvider->pagination,
+        ])?>
+
+    </div>
+
 
 </div>
